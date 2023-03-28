92b0d5bd1c5705b4e13f51566de15041fa2ada1b
Enrick Santos de Souza <enricksantos03@gmail.com>
2023-03-27 21:39:09 -0300

    inicial structure js array solarSystem

---
+++ js/solarSystem.js
@@ -0,0 +1,49 @@
+const planets = [
+  {
+    name: 'Pluto',
+    width: 3.87,
+    height: 3.87
+  },
+  {
+    name: 'Neptune',
+    width: 5.36,
+    height: 5.36
+  },
+  {
+    name: 'Uranus',
+    width: 5.79,
+    height: 5.79
+  },
+  {
+    name: 'Saturn',
+    width: 10.8,
+    height: 9.12
+  },
+  {
+    name: 'Jupiter',
+    width: 7.95,
+    height: 7.95
+  },
+  {
+    name: 'Marte',
+    width: 4.65,
+    height: 4.65
+  },
+  {
+    name: 'Terra',
+    width: 5.58,
+    height: 5.58
+  },
+  {
+    name: 'Venus',
+    width: 4.84,
+    height: 4.84
+  },
+  {
+    name: 'Mercurio',
+    width: 3.99,
+    height: 3.99
+  },
+]
+
+let sizeBorderPlanets = 97.6;
