8c5f7178a5bcaf8fd8b9ba91c85a05da601af4e9
Enrick Santos de Souza <enricksantos03@gmail.com>
2023-03-27 22:47:06 -0300

    update: create system createElement in javascript

--- js/solarSystem.js
+++ js/solarSystem.js
@@ -1,49 +1,83 @@
+
+let sizeBorderPlanets 
+
+// -9.5
 const planets = [
   {
     name: 'Pluto',
     width: 3.87,
-    height: 3.87
+    height: 3.87,
+    alt: 'Planeta Anão Plutão'
   },
   {
     name: 'Neptune',
     width: 5.36,
-    height: 5.36
+    height: 5.36,
+    alt: 'Planeta Netuno'
   },
   {
     name: 'Uranus',
     width: 5.79,
-    height: 5.79
+    height: 5.79,
+    alt: 'Planeta Urano'
   },
   {
     name: 'Saturn',
     width: 10.8,
-    height: 9.12
+    height: 9.12,
+    alt: 'Planeta Saturno'
   },
   {
     name: 'Jupiter',
     width: 7.95,
-    height: 7.95
+    height: 7.95,
+    alt: 'Planeta Jupiter'
   },
   {
     name: 'Marte',
     width: 4.65,
-    height: 4.65
+    height: 4.65,
+    alt: 'Planeta Marte'
   },
   {
     name: 'Terra',
     width: 5.58,
-    height: 5.58
+    height: 5.58,
+    alt: 'Planeta Terra'
   },
   {
     name: 'Venus',
     width: 4.84,
-    height: 4.84
+    height: 4.84,
+    alt: 'Planeta Venus'
   },
   {
     name: 'Mercurio',
     width: 3.99,
-    height: 3.99
+    height: 3.99,
+    alt: 'Planeta Mercurio'
   },
 ]
 
-let sizeBorderPlanets = 97.6;
+
+
+planets.filter((planet) => {
+  let containerSolarSystem = document.querySelector('.solarContainer');
+
+  let containerDiv = document.createElement('div');
+  let contentDiv = document.createElement('div');
+  let img = document.createElement('img');
+
+
+  contentDiv.appendChild(img)
+  containerDiv.appendChild(contentDiv);
+  containerSolarSystem.appendChild(containerDiv);
+
+  
+  containerDiv.className = `containerCircle container${planet.name}Circle`
+  contentDiv.className = `containerPlanets container${planet.name}`
+  img.className = `planet${planet.name}`
+  img.src = `./assets/planets/${planet.name}.svg`
+  img.alt = `${planet.alt}`
+
+})
