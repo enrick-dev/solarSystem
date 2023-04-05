2c99c6ee1edc2bdd349c3d90b0fe604d8c52ed86
Enrick Santos de Souza <enricksantos03@gmail.com>
2023-04-04 21:19:07 -0300

    inicialize reesponsive planets size in js

--- js/solarSystem.js
+++ js/solarSystem.js
@@ -7,8 +7,6 @@ let sizeWindowWidth = window.innerWidth;;
 let calc = sizeWindowWidth / 19.20;
 let calc2 = calc * 0.976
 
-console.log(calc2);
-
 
 const planets = [
   {
@@ -16,63 +14,63 @@ const planets = [
     width: 3.87,
     height: 3.87,
     alt: 'Planeta Anão Plutão',
-    veloc: 7376 / 8
+    veloc: 7376 / 5.2
   },
   {
     name: 'Neptune',
     width: 5.36,
     height: 5.36,
     alt: 'Planeta Netuno',
-    veloc: 4537 / 8
+    veloc: 4537 / 5.2
   },
   {
     name: 'Uranus',
     width: 5.79,
     height: 5.79,
     alt: 'Planeta Urano',
-    veloc: 3006 / 8
+    veloc: 3006 / 5.2
   },
   {
     name: 'Saturn',
     width: 10.8,
     height: 9.12,
     alt: 'Planeta Saturno',
-    veloc: 1503 / 8
+    veloc: 1503 / 5.2
   },
   {
     name: 'Jupiter',
     width: 7.95,
     height: 7.95,
     alt: 'Planeta Jupiter',
-    veloc: 816 / 8
+    veloc: 816 / 5.2
   },
   {
     name: 'Marte',
     width: 4.65,
     height: 4.65,
     alt: 'Planeta Marte',
-    veloc: 249 / 8
+    veloc: 249 / 5.2
   },
   {
     name: 'Terra',
     width: 5.58,
     height: 5.58,
     alt: 'Planeta Terra',
-    veloc: 152 / 8
+    veloc: 152 / 5.2
   },
   {
     name: 'Venus',
     width: 4.84,
     height: 4.84,
     alt: 'Planeta Venus',
-    veloc: 108 / 8
+    veloc: 108 / 5.2
   },
   {
     name: 'Mercurio',
     width: 3.99,
     height: 3.99,
     alt: 'Planeta Mercurio',
-    veloc: 69 / 8
+    veloc: 69 / 5.2
   },
 ]
 
@@ -81,6 +79,7 @@ let topPlanet = [];
 let leftPlanet = [];
 let sizeBorderPlanets = [calc2];
 
+// Criação de toda a construção do sistema Solar
 planets.filter((planet, index) => {
   let containerSolarSystem = document.querySelector('.solarContainer');
 
@@ -125,7 +124,27 @@ planets.filter((planet, index) => {
   planetsAnimation.style.mozAnimation = `spin ${planet.veloc}s linear infinite`;
   planetsAnimation.style.rotate = `${planet.veloc / 2}deg`;
 
-
 })
+
+// Responsividade
+if(sizeWindowWidth < 1920) {
+
+
+  // Tamanho dos Planetas
+  let decreasingValue = 28;
+
+  planets.filter((planet, index) => {
+    let img = document.querySelector(`.planet${planet.name}`);
+    let newWidth = (planet.width - ((planet.width / 100) * decreasingValue)).toFixed(2);
+    let newHeight = (planet.height - ((planet.height / 100) * decreasingValue)).toFixed(2);
+    let newLeft = (newWidth / 2).toFixed(2);
+    let newTop = (newHeight / 2).toFixed(2);
+
+    img.style.width = `${newWidth}rem`;
+    img.style.height = `${newHeight}rem`;
+    img.style.left = `-${newLeft}rem`;
+    img.style.top = `-${newTop}rem`;
+  })
+}
 
 
